ed4a9ba8370e5a55d6345c5fd5ec665d3ffd1433
3maigenkou <kaipurio848@gmail.com>
2023-05-19 10:13:06 +0900

    calコマンドの作成

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -1,43 +1,43 @@
-const argv = require('minimist')(process.argv.slice(2), {
+const argv = require("minimist")(process.argv.slice(2), {
   alias: {
-    y: 'year',
-    m: 'month'
-  }
-})
-const today = new Date()
-const year = argv.y || today.getFullYear()
-const month = argv.m || today.getMonth() + 1
-const wday = ['日', '月', '火', '水', '木', '金', '土']
-const firstDay = new Date(year, month - 1, 1)
-const lastDay = new Date(year, month, 0)
-const dayOfWeek = firstDay.getDay()
+    y: "year",
+    m: "month",
+  },
+});
+const today = new Date();
+const year = argv.y || today.getFullYear();
+const month = argv.m || today.getMonth() + 1;
+const wday = ["日", "月", "火", "水", "木", "金", "土"];
+const firstDay = new Date(year, month - 1, 1);
+const lastDay = new Date(year, month, 0);
+const dayOfWeek = firstDay.getDay();
 const firstDayStr = firstDay
   .getDate()
-  .toLocaleString('ja-JP', { timeZone: 'Asia/Tokyo' })
+  .toLocaleString("ja-JP", { timeZone: "Asia/Tokyo" });
 const lastDayStr = lastDay
   .getDate()
-  .toLocaleString('ja-JP', { timeZone: 'Asia/Tokyo' })
-const days = []
+  .toLocaleString("ja-JP", { timeZone: "Asia/Tokyo" });
+let days = [];
 for (let i = firstDayStr; i <= lastDayStr; i++) {
-  days.push(i)
+  days.push(i);
 }
 for (let i = 0; i <= dayOfWeek; i++) {
-  days.unshift(' ')
+  days.unshift(" ");
 }
 
-console.log(`     ${month}月  ${year}`)
+console.log(`     ${month}月  ${year}`);
 for (let i = 0; i < wday.length; i++) {
-  process.stdout.write(`${wday[i]}  `)
+  process.stdout.write(`${wday[i]} `);
 }
 
 for (let i = 0; i < days.length; i++) {
   if (i % 7 === 0 && days[i] < 10) {
-    process.stdout.write(` ${days[i]}\n`)
+    process.stdout.write(` ${days[i]}\n`);
   } else if (days[i] < 10) {
-    process.stdout.write(` ${days[i]}  `)
+    process.stdout.write(` ${days[i]} `);
   } else if (i % 7 === 0) {
-    process.stdout.write(`${days[i]}\n`)
+    process.stdout.write(`${days[i]}\n`);
   } else {
-    process.stdout.write(`${days[i]}  `)
+    process.stdout.write(`${days[i]} `);
   }
 }
